08b0b6da1fe0cdecbe6a611e2aa67dc982b2ed41
kalilakorin <kalila.korin@gmail.com>
2025-04-20 19:51:24 -0600

    Assignment 13a - logging

---
+++ assignment13/log_exercise.py
@@ -0,0 +1,13 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    filename='app.log',
+                    filemode='w',
+                    format='%(name)s - '
+                           '%(levelname)s - '
+                           '%(message)s')
+
+logging.debug('This is a debug statement')
+logging.info('This will get logged to a file')
+logging.warning('This is a Warning')
+logging.error('This is an error!')
